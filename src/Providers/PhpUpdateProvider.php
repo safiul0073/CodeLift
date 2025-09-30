8a00ee8ad71ddb72ec7a5ba9aa08a1d2919b614e
safiul007 <mdsafiul0073@gmail.com>
2025-09-30 12:58:51 +0600

    config publish issue resolved

--- src/Providers/PhpUpdateProvider.php
+++ src/Providers/PhpUpdateProvider.php
@@ -11,7 +11,7 @@ class PhpUpdateProvider extends ServiceProvider
     public function boot()
     {
         $this->mergeConfigFrom(
-            __DIR__.'/../config/lifter.php', 'lifter'
+            __DIR__.'/../../config/lifter.php', 'lifter'
         );
 
         $this->app->bind(Version::class, VersionUpdate::class);
@@ -20,7 +20,7 @@ class PhpUpdateProvider extends ServiceProvider
     public function register()
     {
         $this->publishes([
-            __DIR__.'/../config/lifter.php' => config_path('lifter.php'),
+            __DIR__.'/../../config/lifter.php' => config_path('lifter.php'),
         ], 'lifter-config');
     }
 }
